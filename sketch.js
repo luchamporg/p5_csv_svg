6ff1b9713cd83874a0a790813614991631f26c9c
luisramq <luisramz@gmail.com>
2023-01-26 15:11:46 -0500

    Mostrar datos
    Se muestran los datos del archivos CSV en el canvas

--- sketch.js
+++ sketch.js
@@ -1,5 +1,12 @@
-//Función para cargar archivos
+//Variables para crear las celdas de las tablas
+let cellWidth;
+let cellHeight;
+const margen = 200;
+
+//Variable para cargar los datos del CSV
 let data;
+
+//Función para cargar archivos
 function preload() {
   data = loadTable("./src/30canciones.csv", "csv", "header");
 }
@@ -7,10 +14,30 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight, SVG);
 
+  //El ancho de la pantalla dividido para el número de columnas
+  cellWidth = (width - margen) / data.columns.length;
+
+  //El alto de la pantalla dividido para el número de filas
+  cellHeight = (height - margen) / data.rows.length;
+
   //Verifica que se cargan los datos del CSV
-  print(data.rows);
+  print(cellWidth, cellHeight);
 }
 
 function draw() {
-  circle(mouseX, mouseY, 20);
+  //circle(mouseX, mouseY, 20);
+
+  for (let i = 0; i < data.columns.length; i++) {
+    const x = i * cellWidth + margen / 2;
+    for (let j = 0; j < data.rows.length; j++) {
+      const y = j * cellHeight + margen / 2;
+      fill(125);
+      noStroke();
+      //circle(x, y, 10);
+      const d = data.rows[j].arr[i];
+      text(d, x, y);
+    }
+  }
+
+  noLoop();
 }
